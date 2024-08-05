2efb49fbc9fa33faa4c1f2647734f64595532b5f
MinnKhantZ <minkhankzaw2543@gmail.com>
2024-08-05 22:20:24 +0630

    postgresql fixed

--- src/main/resources/schema.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE employee (
-    number SERIAL PRIMARY KEY,
-    role CHAR(50) NOT NULL,
-    salary DOUBLE PRECISION
-);
--- target/classes/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO employee (role, salary) VALUES ('Manager', 75000.00);
-INSERT INTO employee (role, salary) VALUES ('Developer', 60000.00);
